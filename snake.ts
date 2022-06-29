4a348dd7ddcd13a562c59a32f28fd7f029880fc5
Tage-official <maksim.grincevich@gmail.com>
2022-06-29 15:58:19 +0300

    flexible field, removed everything except matrix && environment

--- snake.ts
+++ snake.ts
@@ -1,69 +1,15 @@
-type matrix = [[]]
-
-const max:number = 15
+const readline:any = require('readline-sync')
+const max:number = readline.question('Enter field size: ') 
 const min:number = 0
 
-interface Point{
-
+interface Matrix {
+    ground:[[]];
 }
 
-interface Matrix{
-    ground:matrix;
+interface Environment {
+    value: ' '
 }
 
-interface Environment{
-
-}
-
-interface EnvironmentField{
-
-}
-
-interface Snake{
-    value:string;
-    x:number;
-    y:number;
-}
-
-let head: Snake = {
-    value: '@',
-    x: 8,
-    y: 8
-}
-
-interface SnakeBody{
-    value: string;
-    x: number;
-    y: number;
-}
-
-let body: SnakeBody = {
-    value: '=',
-    x: 8,
-    y: 8
-}
-
-interface Food{
-    x: number;
-    y: number;
-}
-
-let food: Food = {
-    x: Math.floor(Math.random() * (max - min) + min),
-    y: Math.floor(Math.random() * (max - min) + min)
-}
-
-interface GameController{
-
-}
-
-interface SnakeController{
-
-}
-
-interface KeyboardController{
-    w:string;
-    a:string;
-    s:string;
-    d:string;
+interface EnvironmentField {
+    value:'#'
 }
