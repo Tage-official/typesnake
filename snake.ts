68914eb31d2da6d667779b61526f30c91e3f0c6f
Tage-official <maksim.grincevich@gmail.com>
2022-06-30 03:53:52 +0300

    added 'lvl1', edited displaymatrix

--- snake.ts
+++ snake.ts
@@ -1,3 +1,5 @@
+import { arrayBuffer } from "stream/consumers";
+
 interface Matrix {
     matrix: string[][];
     min: number;
@@ -18,30 +20,176 @@ interface Point {
     y: number
 }
 
-const field: EnvironmentField = {
-    value: '#',
-    coordinate: {x:0, 
-                 y:15
-    }
-}
-
 let ground: Matrix = {
     matrix: [[]],
     min: 0,
-    max: 15
+    max: 16
+}
+const lvl1 : Environment = {
+    value: [{value:'#',
+        coordinate:{
+            x:1,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:2,
+            y:0
+        }},{
+        value:'#',
+        coordinate:{
+            x:3,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:4,
+            y:0
+        }},
+        {value:'#',
+        coordinate:{
+            x:5,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:6,
+            y:0
+        }},{
+        value:'#',
+        coordinate:{
+            x:7,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:8,
+            y:0
+        }},{
+        value:'#',
+        coordinate:{
+            x:9,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:10,
+            y:0
+        }},{
+        value:'#',
+        coordinate:{
+            x:11,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:12,
+            y:0
+        }},{
+        value:'#',
+        coordinate:{
+            x:13,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:14,
+            y:0
+        }},{
+        value:'#',
+        coordinate:{
+            x:15,
+            y:0,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:1
+        }},{
+        value:'#',
+        coordinate:{
+            x:0,
+            y:2,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:3
+        }},{
+        value:'#',
+        coordinate:{
+            x:0,
+            y:4,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:5
+        }},{
+        value:'#',
+        coordinate:{
+            x:0,
+            y:6,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:7
+        }},{
+        value:'#',
+        coordinate:{
+            x:0,
+            y:8,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:9
+        }},{
+        value:'#',
+        coordinate:{
+            x:0,
+            y:10,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:11
+        }},{
+        value:'#',
+        coordinate:{
+            x:0,
+            y:12,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:13
+        }},{
+        value:'#',
+        coordinate:{
+            x:0,
+            y:14,
+        },
+    }, {value:'#',
+        coordinate:{
+            x:0,
+            y:15
+        }}
+]
+
 }
 
-    function displayMatrix(){
-        for (var i = 0; i < ground.max; i++){
-            for (var j = 0; j < ground.max; j++ ){
-              if (i == field.coordinate.x || j == field.coordinate.x || i == field.coordinate.y || j == field.coordinate.y){
-                ground.matrix[i][j] = field.value
-              }
-              else{
-                ground.matrix[i][j] = ' ' 
-              }
-            }
+    function displayMatrix( ) {
+        let x = Array(ground.max);
+        for ( let i=0; i < x.length; i += 1 ) {
+            x[i] = Array(ground.max);
         }
+        ground.matrix = x
     }
-    console.log(ground.matrix)
-    displayMatrix()
+
+    function addEnvironmentToMatrix( ) {
+    }
+
+displayMatrix()
+console.log(ground.matrix)
